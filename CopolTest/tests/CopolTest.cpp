2bb8ac31d1d2c33ca4d6386e8667b29ff15c6813
Hugo van den Brand <h.vd.brand@gmail.com>
2016-05-21 22:44:03 +0200

    * Added tests on number of units

--- CopolTest/tests/CopolTest.cpp
+++ CopolTest/tests/CopolTest.cpp
@@ -23,13 +23,30 @@ void PolymerChain_CallGetLength_ReturnsOne()
 	BOOST_CHECK_EQUAL(1,len);
 }
 
+void PolymerChain_GetNumberOfunits_ReturnsZero()
+{
+	PolymerChain test(4);
+	unsigned int units=test.get_units_of_type(3);
+	BOOST_CHECK_EQUAL(0,units);
+}
+
+void PolymerChain_GetNumberOfunits_ReturnsOne()
+{
+	PolymerChain test(4);
+	unsigned int units=test.get_units_of_type(4);
+	BOOST_CHECK_EQUAL(1,units);
+}
+
 test_suite* init_unit_test_suite( int /*argc*/, char* /*argv*/[] )
 {
    framework::master_test_suite().
     add( BOOST_TEST_CASE( &free_test_function ) );
    framework::master_test_suite().
        add( BOOST_TEST_CASE( &PolymerChain_CallGetLength_ReturnsOne ) );
-
+   framework::master_test_suite().
+          add( BOOST_TEST_CASE( &PolymerChain_GetNumberOfunits_ReturnsZero ) );
+   framework::master_test_suite().
+          add( BOOST_TEST_CASE( &PolymerChain_GetNumberOfunits_ReturnsOne ) );
   return 0;
 }
 
