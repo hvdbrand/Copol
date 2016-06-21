a66274d3cf019976813da8fe10b69285824617c4
Hugo van den Brand <h.vd.brand@gmail.com>
2016-06-21 23:18:27 +0200

    * Added BaseMolecules tests

--- CopolTest/tests/BaseMoleculesTests.cpp
+++ CopolTest/tests/BaseMoleculesTests.cpp
@@ -8,9 +8,41 @@
 #include "gtest/gtest.h"
 #include "../src/main.h"
 
-namespace BaseMoleculeTests {
+namespace BaseMoleculesTests {
 
+BaseMolecules EmptyBaseMolecules() {
+	return BaseMolecules();
+}
+
+TEST(BaseMoleculesTests,NumberOfMolecules_Initialized_ReturnsZero)
+{
+	BaseMolecules basemol=EmptyBaseMolecules();
+	ASSERT_EQ(basemol.number_of_molecules(),0);
 };
 
+TEST(BaseMoleculesTests,NameInDb_Initialized_ReturnsMinusOne)
+{
+	BaseMolecules basemol=EmptyBaseMolecules();
+	ASSERT_EQ(basemol.name_in_db("foo",5),0);
+};
 
+TEST(BaseMoleculesTests,GetName_Initialized_ReturnsEmptyString)
+{
+	BaseMolecules basemol=EmptyBaseMolecules();
+	ASSERT_EQ(basemol.get_name(5),"");
+};
+
+TEST(BaseMoleculesTests,GetType_Initialized_ReturnsZero)
+{
+	BaseMolecules basemol=EmptyBaseMolecules();
+	ASSERT_EQ(basemol.get_type(5),0);
+};
+
+TEST(BaseMoleculesTests,GetMass_Initialized_ReturnsZero)
+{
+	BaseMolecules basemol=EmptyBaseMolecules();
+	ASSERT_EQ(basemol.get_mass(5),0);
+};
+
+}
 
